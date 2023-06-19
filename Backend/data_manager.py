c82d1fcdfdcc0d292a83094ea6125a59c576bc62
Karim-ch07 <chihanikarim07@gmail.com>
2023-06-19 10:42:37 +0200

    changed code for server hosting

--- Backend/data_manager.py
+++ Backend/data_manager.py
@@ -1,48 +1,76 @@
 import os
-import requests
+import mysql.connector
+import time
 
-USER_ID = os.environ.get("USER_IDENTIFICATION")
-TOKEN = os.environ.get("SHEETY_AUTH_TOKEN")
 
-SHEETY_PRICES_ENDPOINT = f"https://api.sheety.co/{USER_ID}/flightDeals/prices"
-SHEETY_USERS_ENDPOINT = f"https://api.sheety.co/{USER_ID}/flightDeals/users"
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = "Lala-2023"
+DB_NAME = "flightdb"
+DB_PORT = "3306"
 
-sheety_headers = {
-    "Authorization": TOKEN,
-}
+# Establish the database connection
+max_retries = 2
+retry_interval = 10  # seconds
 
 
 class DataManager:
-
     def __init__(self):
+        self.db = self.establish_db_connection()
         self.dest_info = []
         self.cx_info = []
 
+
+
+    def establish_db_connection(self):
+        for attempt in range(max_retries):
+            try:
+                # Attempt to establish the MySQL database connection
+                self.db = mysql.connector.connect(
+                    host=DB_HOST,
+                    user=DB_USER,
+                    password=DB_PASSWORD,
+                    database=DB_NAME,
+                    port=DB_PORT
+                )
+                return self.db
+
+            except mysql.connector.Error as e:
+                # Check if retry conditions are met
+                if attempt < max_retries - 1:
+                    # Calculate next retry interval (e.g., fixed interval)
+                    retry_delay = retry_interval
+
+                    # Wait for the retry interval
+                    time.sleep(retry_delay)
+
     def get_dest_info(self):
-        # GET REQUEST
-        # get info from Google sheet
-        response = requests.get(url=SHEETY_PRICES_ENDPOINT, headers=sheety_headers)
-        response.raise_for_status()
-        self.dest_info = response.json()["prices"]
+        # Retrieve data from the MySQL database
+        cursor = self.db.cursor()
+        cursor.execute("SELECT * FROM prices")
+        self.dest_info = cursor.fetchall()
+        cursor.close()
         return self.dest_info
 
     def update_dest_info_iata(self):
-        # PUT REQUEST
-        # update info in the Google sheet
+        # Update data in the MySQL database
+        cursor = self.db.cursor()
         for dest in self.dest_info:
-            SHEETY_PUT_ENDPOINT = f"{SHEETY_PRICES_ENDPOINT}/{dest['id']}"
-            dest_update = {
-                "price": {
-                    "iataCode": dest["iataCode"],
-                }
-            }
-            response = requests.put(url=SHEETY_PUT_ENDPOINT, json=dest_update, headers=sheety_headers)
-            response.raise_for_status()
+            sql = "UPDATE prices SET IATA = %s WHERE id = %s"
+            val = (dest[2], dest[0])
+            cursor.execute(sql, val)
+            db.commit()
+        cursor.close()
 
-    def get_cx_info(self):
-        # GET REQUEST
-        # get info from Google sheet
-        response = requests.get(url=SHEETY_USERS_ENDPOINT, headers=sheety_headers)
-        response.raise_for_status()
-        self.cx_info = response.json()["users"]
-        return self.cx_info
+    # def get_cx_info(self):
+    #     # Retrieve data from the MySQL database
+    #     cursor = db.cursor()
+    #     cursor.execute("SELECT * FROM users")
+    #     self.cx_info = cursor.fetchall()
+    #     cursor.close()
+    #     return self.cx_info
+
+
+# data_manager = DataManager()
+# response = data_manager.get_dest_info()
+# print(response)
--- Backend/main.py
+++ Backend/main.py
@@ -1,18 +1,24 @@
 from datetime import *
 from flight_search import FlightSearch
 from data_manager import DataManager
-from notification_manager import NotificationManager
+import json
+from pprint import pprint
+#from flask import Flask, request, jsonify
+
+# app = Flask(__name__)
+#
+# @app.route("/")
 
 data_manager = DataManager()
 sheet_dest_data = data_manager.get_dest_info()
-sheet_cx_data = data_manager.get_cx_info()
+#sheet_cx_data = data_manager.get_cx_info()
 flight_search = FlightSearch()
-notification = NotificationManager()
+#notification = NotificationManager()
 
 update_needed = False
 for dest in sheet_dest_data:
-    if dest["iataCode"] == "":
-        dest["iataCode"] = flight_search.get_iata(dest["city"])
+    if dest[2] == "":
+        dest[2] = flight_search.get_iata(dest[1])
         update_needed = True
 
 if update_needed:
@@ -22,13 +28,13 @@ if update_needed:
 DEPARTURE_IATA = "POZ"
 tomorrow = datetime.now() + timedelta(days=1)
 six_months_later = tomorrow + timedelta(days=180)
-emails = [cx_data["email"] for cx_data in sheet_cx_data]
-
+#emails = [cx_data["email"] for cx_data in sheet_cx_data]
+results = []
 for dest in sheet_dest_data:
     # search flights
     flight = flight_search.search_flights(
         fly_from=DEPARTURE_IATA,
-        fly_to=dest["iataCode"],
+        fly_to=dest[2],
         date_from=tomorrow,
         date_to=six_months_later
     )
@@ -37,29 +43,44 @@ for dest in sheet_dest_data:
     if flight is None:
         continue
 
-    if flight.lowestPrice < dest["lowestPrice"]:
+    if flight.lowestPrice is not None:
+        # to_send = f"Low Price Alert! Only {flight.lowestPrice} PLN to fly from " \
+        #           f"{flight.departure_city} | {flight.departure_airport_code} " \
+        #           f"to {flight.dest_city} | {flight.dest_airport_code}, " \
+        #           f"from {flight.outbound_date} to {flight.inbound_date}.\n"
+        # link = f"\nhttps://www.google.com/travel/flights?q=Flights%20to%20" \
+        #        f"{flight.dest_airport_code}%20from%20{flight.departure_airport_code}" \
+        #        f"%20on%20{flight.outbound_date}%20through%20{flight.inbound_date}"
+        #
+        # if flight.stop_overs == 0:
+        #     print(f"{to_send}\n{link}")
+        # else:
+        #     stop_over = f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city} City.\n"
+        #     print(f"{to_send + stop_over}\n{link}")
         to_send = f"Low Price Alert! Only {flight.lowestPrice} PLN to fly from " \
                   f"{flight.departure_city} | {flight.departure_airport_code} " \
                   f"to {flight.dest_city} | {flight.dest_airport_code}, " \
-                  f"from {flight.outbound_date} to {flight.inbound_date}.\n"
-        link = f"\nhttps://www.google.com/travel/flights?q=Flights%20to%20" \
+                  f"from {flight.outbound_date} to {flight.inbound_date}."
+        link = f"https://www.google.com/travel/flights?q=Flights%20to%20" \
                f"{flight.dest_airport_code}%20from%20{flight.departure_airport_code}" \
                f"%20on%20{flight.outbound_date}%20through%20{flight.inbound_date}"
 
         if flight.stop_overs == 0:
-            notification.send_sms(
-                message=to_send
-            )
-            notification.send_emails(
-                body=to_send+link,
-                emails=emails
-            )
+            result = {
+                "message": to_send,
+                "link": link
+            }
         else:
-            stop_over = f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city} City.\n"
-            notification.send_sms(
-                message=to_send+stop_over
-            )
-            notification.send_emails(
-                body=to_send+stop_over+link,
-                emails=emails
-            )
+            stop_over = f"Flight has {flight.stop_overs} stop over, via {flight.via_city} City."
+            result = {
+                "message": f"{to_send}\n{stop_over}",
+                "link": link
+            }
+    results.append(result)
+#results = json.dumps(result, indent=4)
+pprint(results)
+
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=3000)
+
